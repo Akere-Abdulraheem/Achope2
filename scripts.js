e996e5edc031a7df083863c19fc501d6fdd35de8
Akere-Abdulraheem <113618444+Akere-Abdulraheem@users.noreply.github.com>
2023-09-04 19:22:12 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// All variables are declared here otherwise needed inside a function
+    const networkSelect = document.getElementById("network");
+    const amountSelect = document.getElementById("amount");
+    const dataValueDisplay = document.getElementById("dataValueDisplay");
+    const submitOrderButton = document.getElementById("submitOrder");
+    const overlay = document.getElementById("overlay");
+    const phoneNumberInput = document.getElementById("phoneNumber");
