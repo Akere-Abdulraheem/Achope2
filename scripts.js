eee08080d60edb724fc9352ed2ff6f4ef10a5a04
Akere-Abdulraheem <113618444+Akere-Abdulraheem@users.noreply.github.com>
2023-09-04 19:25:01 +0100

    Update scripts.js - price data created

--- scripts.js
+++ scripts.js
@@ -5,3 +5,11 @@
     const submitOrderButton = document.getElementById("submitOrder");
     const overlay = document.getElementById("overlay");
     const phoneNumberInput = document.getElementById("phoneNumber");
+
+// All price data are now created here as array
+    const priceList = {
+        "9MOBILE": [250, 420, 580, 740, 1900],
+        "AIRTEL": [300, 550, 950, 1200, 2250],
+        "GLO": [300, 550, 800, 1200, 2250],
+        "MTN": [300, 550, 800, 1400, 2500]
+    };   
