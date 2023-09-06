6f4196a73b71efc95b693ec132b5e7c2f2df4430
Akere-Abdulraheem <113618444+Akere-Abdulraheem@users.noreply.github.com>
2023-09-06 23:14:23 +0100

    Update verifyPayment.js

--- functions/verifyPayment.js
+++ functions/verifyPayment.js
@@ -1,26 +1,33 @@
-// Import necessary dependencies here if needed
-// const axios = require('axios');
+const axios = require('axios');
 
 exports.handler = async (event, context) => {
   try {
     // Parse the POST data from the request body
     const { network, amount, reference } = JSON.parse(event.body);
 
-    // Perform the payment verification logic here
-    // You can make an API call to Paystack's API to verify the payment
-    // Example:
-    // const response = await axios.post('https://api.paystack.com/verify_transaction', {
-    //   reference: reference,
-    //   amount: amount * 100, // Convert amount to kobo
-    // });
+    // Make an API call to Paystack's verify_transaction endpoint
+    const paystackResponse = await axios.post('https://api.paystack.com/transaction/verify/' + reference, null, {
+      headers: {
+        Authorization: 'Bearer YOUR_PAYSTACK_SECRET_KEY', // Replace with your Paystack secret key
+      },
+    });
 
     // Check if the payment is successful in the response
-
-    // Send a response based on the verification result
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Payment verification successful' }),
-    };
+    if (paystackResponse.data.status === true) {
+      // Payment was successful
+      // You can perform additional actions here, such as updating your database
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Payment verification successful' }),
+      };
+    } else {
+      // Payment failed
+      // Handle the error scenario
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: 'Payment verification failed' }),
+      };
+    }
   } catch (error) {
     console.error('Error:', error);
     return {
