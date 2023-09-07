890cdcbc81970c1028d6461e692e716403a0658f
Akere-Abdulraheem <113618444+Akere-Abdulraheem@users.noreply.github.com>
2023-09-07 14:33:27 +0100

    Update sendSMS.js

--- functions/sendSMS.js
+++ functions/sendSMS.js
@@ -11,18 +11,19 @@ app.use(bodyParser.json());                // Parse JSON data in request bodies.
 const accountSid = process.env.TWILIO_ACCOUNT_SID;    // Your Twilio Account SID.
 const authToken = process.env.TWILIO_AUTH_TOKEN;      // Your Twilio Auth Token.
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER; // Your Twilio phone number.
+const twilioRecipientPhoneNumber = process.env.TWILIO_RECIPIENT_PHONE_NUMBER; // Your Twilio phone number.
 
 // Create a Twilio client instance.
 const client = new twilio(accountSid, authToken);
 
 // Define a route for handling SMS sending.
 app.post('/send-sms', (req, res) => {
-    const { phoneNumber, message } = req.body; // Extract phone number and message from the request body.
+    const { message } = req.body; // Extract phone number and message from the request body.
 
     // Use the Twilio client to send an SMS message.
     client.messages.create({
         body: message,                  // Message content.
-        to: phoneNumber,               // Recipient's phone number.
+        to: twilioRecipientPhoneNumber, // Recipient's phone number.
         from: twilioPhoneNumber,       // Your Twilio phone number.
     })
     .then(message => {
