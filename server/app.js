d79011e9e5399b710919da665f23bcdf026ac6ef
Abdulraheem Akere <akereodunayo@gmail.com>
2023-10-04 17:05:36 +0100

    some relief twilio works with the submit button Hurray!

--- server/app.js
+++ server/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 const routes = require('./routes/router');
 app.set('view engine', 'ejs');
 app.set('views', 'server');
 app.use(express.static('public'));
 app.use('/',routes);
-app.listen(port, () => {
-    console.log(`server is running port${port}`);
+
+const twilio = require('twilio');
+const dotenv = require('dotenv').config();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Twilio Credentials
+const accountSid = process.env.YOUR_TWILIO_ACCOUNT_SID;
+const authToken = process.env.YOUR_TWILIO_AUTH_TOKEN;
+const twilioPhoneNumber = process.env.YOUR_TWILIO_PHONE_NUMBER;
+
+// Initialize Twilio client
+const client = new twilio(accountSid, authToken);
+
+// Routes
+const router = require('./routes/router');
+app.use('/', router);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
